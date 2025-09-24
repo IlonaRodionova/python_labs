8e9f72a62acf37e6a7412e7ace521b9b9c3fc9ce
Ilona <ilonarodionova777@gmail.com>
2025-09-24 15:37:15 +0300

    lab02 commit

--- src/lab01/05_initials_and_len.py
+++ src/lab01/05_initials_and_len.py
@@ -1,6 +1,6 @@
 fio = input('ФИО: ')
 ini = fio.split()
-fio =  fio.replace(' ', '')
+fio = fio.replace(' ', '')
 i = ini[0][:1] + ini[1][:1] + ini[2][:1]
 print(f'Инициалы: {i}')
 print(f'Длина (символов): {len(fio)+2}')
