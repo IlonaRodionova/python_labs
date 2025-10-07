1d37178305fb05d12b2bcd87dc2d439aa9b1407d
Ilona <ilonarodionova777@gmail.com>
2025-10-07 12:50:34 +0300

    lab02 commit

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -60,3 +60,12 @@ print(min_max([1.5, 2, 2.0, -3.1]))
 
 print("unique_sorted:")
 print(unique_sorted([3, 1, 2, 1, 3]))
+print(unique_sorted([]))
+print(unique_sorted([-1, -1, 0, 2, 2]))
+print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
+print("flatten")
+print(flatten([[1, 2], [3, 4]]))
+print(flatten(([1, 2], (3, 4, 5))))
+print(flatten([[1], [], [2, 3]]))
+print(flatten([[1, 2], "ab"]))
