562866efc5616485939a92e12666ee66f1573172
Ilona <ilonarodionova777@gmail.com>
2025-10-21 13:17:23 +0300

    lab02 commit

--- src/lab03/README.md
+++ src/lab03/README.md
@@ -51,11 +51,29 @@ tok_2 = ["bb", "aa", "bb", "aa", "cc"]
 freq_2 = count_freq(tok_2)
 print(top_n(freq_2, n=2))
 ```
-
+![text.png](..%2F..%2Fimages%2Flab03%2Ftext.png)
 ## Задание 2
 
 ```
+# sys.path.append(r'C:\Users\Илона\Desktop\python_labs\src\lib')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib'))) # относительный путь к файлу (позволяет удобно использовать данный код на различных устройствах)
+from text import normalize, tokenize, count_freq, top_n
 
+def main():
+    a = sys.stdin.read()
+    total_words = tokenize(normalize(a))
+    unique_words = set(total_words)
+    top_words = top_n(count_freq(total_words))
+    print(f"Всего слов: {len(total_words)}")
+    print(f"Уникальных слов: {len(unique_words)}")
+    print("Топ-5:")
+    for word, count in top_words:
+        print(f"{word}: {count}")
+
+if __name__ == "__main__":
+    main()
+
+# echo "Hello world!!! Hello!" | python src/lab03/text_stats.py
 ```
-
+![text_stats.png](..%2F..%2Fimages%2Flab03%2Ftext_stats.png)
 
--- src/lab03/text_stats.py
+++ src/lab03/text_stats.py
@@ -1,28 +1,22 @@
-from lib.text import normalize, tokenize, count_freq, top_n
 import sys
+import os
+
+# sys.path.append(r'C:\Users\Илона\Desktop\python_labs\src\lib')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib'))) # относительный путь к файлу (позволяет удобно использовать данный код на различных устройствах)
+from text import normalize, tokenize, count_freq, top_n
 
 def main():
-    text = sys.stdin.buffer.read().decode('utf-8')  # вход к бинарным данным,преобразует строку в юникод
-    if not text.strip():
-        print("Нет входных данных")
-        return
-    normalized_text = normalize(text)
-    tokens = tokenize(normalized_text)
-
-    if not tokens:
-        print("В тексте не найдено слов")
-        return
-
-    total_words = len(tokens)  # общее количество слов
-    freq_dict = count_freq(tokens)  # словарь частот
-    unique_words = len(freq_dict)  # количеситво уникальных слов
-    top_words = top_n(freq_dict, 5)  # самые популярные частоты
-
-    print(f"Всего слов: {total_words}")
-    print(f"Уникальных слов: {unique_words}")
+    a = sys.stdin.read()
+    total_words = tokenize(normalize(a))
+    unique_words = set(total_words)
+    top_words = top_n(count_freq(total_words))
+    print(f"Всего слов: {len(total_words)}")
+    print(f"Уникальных слов: {len(unique_words)}")
     print("Топ-5:")
     for word, count in top_words:
         print(f"{word}: {count}")
 
 if __name__ == "__main__":
     main()
+
+# echo "Hello world!!! Hello!" | python src/lab03/text_stats.py
